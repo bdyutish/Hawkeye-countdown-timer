055c3287cd41ce3c42c1f894daaceea6e7a1eaae
Dyutish Bandyopadhyay <49207345+bdyutish@users.noreply.github.com>
2020-04-26 13:29:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var deadline = new Date("mar 27, 2020 00:00:00").getTime(); 
+var deadline = new Date("may9, 2020 00:00:00").getTime(); 
   
 var x = setInterval(function() { 
   
@@ -8,7 +8,7 @@ var days = Math.floor(t / (1000 * 60 * 60 * 24));
 var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
 var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
 var seconds = Math.floor((t % (1000 * 60)) / 1000); 
-document.getElementById("day").innerHTML =days ; 
+document.getElementById("day").innerHTML =days; 
 document.getElementById("hour").innerHTML =hours; 
 document.getElementById("minute").innerHTML = minutes;  
 document.getElementById("second").innerHTML =seconds;  
