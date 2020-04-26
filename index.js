af8305809c52794d1ea183545e6ca91ae6e5dbd6
Dyutish Bandyopadhyay <49207345+bdyutish@users.noreply.github.com>
2020-04-27 01:53:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var days = Math.floor(t / (1000 * 60 * 60 * 24));
 var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
 var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
 var seconds = Math.floor((t % (1000 * 60)) / 1000); 
-document.getElementById("day").innerHTML =days; 
-document.getElementById("hour").innerHTML =hours; 
-document.getElementById("minute").innerHTML = minutes;  
-document.getElementById("second").innerHTML =seconds;  
+document.getElementById("day").innerHTML =days<10?'0'+days:days; 
+document.getElementById("hour").innerHTML =hours<10?'0'+hours:hours; 
+document.getElementById("minute").innerHTML = minutes<10?'0'+minutes:minutes;  
+document.getElementById("second").innerHTML =seconds<10?'0'+seconds:seconds;  
 if (t < 0) { 
         clearInterval(x); 
         document.getElementById("timeup").innerHTML = "TIME UP"; 
